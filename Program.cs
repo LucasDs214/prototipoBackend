fcea4b740c3838947dde8566f39b272523caf967
Lucas Santos <lucas.santos@faetec.quintino.rj>
2026-02-27 17:38:57 -0300

    conexão final

--- Program.cs
+++ Program.cs
@@ -6,10 +6,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Configuração do Supabase (Vindo da pasta Config)
 builder.Services.AddSupabaseConfig();
 
-// Configuração de CORS
-builder.Services.AddCors(options => 
-    options.AddDefaultPolicy(p => p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod())
-);
+// Procure a parte do builder.Services.AddCors e deixe assim:
+builder.Services.AddCors(options => {
+    options.AddDefaultPolicy(policy => {
+        policy.WithOrigins("https://prot-tipo2-0.vercel.app") // Use o link que a Vercel te deu
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 app.UseCors();
